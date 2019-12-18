956885258f6cebe61737726f0133a5aa2e1a3cf0
Balal Naeem <balalnaeem@gmail.com>
2019-12-18 12:28:29 +0000

    final update

--- lesson_2/rot_13.js
+++ lesson_2/rot_13.js
@@ -1,28 +1,3 @@
-/*
-input: string
-output: string
-
-Rules:
-- case preservation
-- non alphabet letters stay the same
-
-Problem:
-- create an array and play around with it to understand the index
-- so basically get the index of current letter
-- if current index + 13 is bigger than 25, user modulo 13
-- 26 % 13 = this will be new index
-
-Al:
-- declare a function that takes a string as an argument
-- convert the string into an array of characters
-- iterate over the array with map
-- check if character belongs to lowercase or uppercase
-- get the index in the array, calculate the new index and return that character
-- join the returning array from map into a string
-- return that string
-
-*/
-
 let lowerCaseLetters = []
 let upperCaseLetters = []
 
@@ -57,31 +32,10 @@ function rot13(string) {
                         return letter;
                       }
                     });
+
   return new_letters.join('');
 }
 
 console.log(rot13(rot13('Teachers open the door, but you must enter by yourself.')));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Logs : Teachers open the door, but you must enter by yourself.
